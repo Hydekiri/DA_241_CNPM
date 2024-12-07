5418acaf1815ac87fe622abc09820b2a9be3c0ef
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2024-12-07 22:13:22 +0700

    adding authorization

--- be/src/routers/receiverMessageRounter.js
+++ be/src/routers/receiverMessageRounter.js
@@ -1,20 +1,22 @@
 module.exports = (router) => {
     const receiverMessageController = require("../controllers/receiverMessageController");
 
+    const middlewareController = require("../controllers/middlewareController");
+
     // Gửi thông báo đến một người dùng cụ thể
-    router.post("/notifications/send", receiverMessageController.sendNotificationToUser);
+    router.post("/notifications/send", middlewareController.verifyAdmin, receiverMessageController.sendNotificationToUser);
 
     // Lấy danh sách người nhận của một thông báo
-    router.get("/notifications/:notificationId/receivers", receiverMessageController.getReceiversByNotification);
+    router.get("/notifications/:notificationId/receivers", middlewareController.verifyAdmin, receiverMessageController.getReceiversByNotification);
 
     // Lấy danh sách thông báo được gửi đến một người dùng
-    router.get("/users/:userId/notifications", receiverMessageController.getNotificationsForUser);
+    router.get("/users/:userId/notifications", middlewareController.verifyAdmin, receiverMessageController.getNotificationsForUser);
 
     // Xóa một người nhận khỏi thông báo
-    router.delete("/notifications/:notificationId/receivers/:userId", receiverMessageController.removeReceiverFromNotification);
+    router.delete("/notifications/:notificationId/receivers/:userId", middlewareController.verifyAdmin, receiverMessageController.removeReceiverFromNotification);
 
     // Gửi thông báo đến tất cả người dùng
-    router.post("/notifications/sendToAll", receiverMessageController.sendNotificationToAllUsers);
+    router.post("/notifications/sendToAll", middlewareController.verifyAdmin, receiverMessageController.sendNotificationToAllUsers);
 };
 
 /* 
