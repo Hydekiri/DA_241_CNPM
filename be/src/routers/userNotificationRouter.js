f863a391b7857500410d6d3188c0b5bca4655761
DANG VAN TAN <161315905+tank22BKU@users.noreply.github.com>
2024-12-07 21:57:15 +0700

    add authorization for getting Notifications
    Admin can get any notification of any Student
    Student can only get their notifications

--- be/src/routers/userNotificationRouter.js
+++ be/src/routers/userNotificationRouter.js
@@ -1,6 +1,7 @@
 module.exports = (router) => {
     const userNotificationController = require("../controllers/userNotificationController");
+    const middlewareController = require("../controllers/middlewareController");
 
     // API lấy danh sách chi tiết thông báo của một user
-    router.get("/users/:userId/notifications/details", userNotificationController.getUserNotificationsWithDetails );
+    router.get("/users/:userId/notifications/details", middlewareController.verifyControllerAndAdmin, userNotificationController.getUserNotificationsWithDetails );
 };
